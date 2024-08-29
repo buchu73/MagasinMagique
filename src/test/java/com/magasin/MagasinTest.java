971ac3c9b0809a7bbeecef77f9ed61dc78298c66
Damien LOBATO <damien.lobato@Digital-Grenoble.local>
2024-08-29 10:54:21 +0200

    ParameterizedTest

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -3,23 +3,28 @@ package com.magasin;
 import java.util.Random;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
     Random random = new Random();
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({
+            "Kryptonite, 80, 80", //To test if the quality and the sellIn are still the same
+            "Kryptonite, 30, 35", //To test if the quality must always be 80
+    })
+    void Kryptonite(String name, int sellIn, int quality) {
 
-    void Kryptonite() {
-        int sellIn = random.nextInt(80);
-        int quality = random.nextInt(80);
 
-        Item[] items = new Item[] { new Item("Kryptonite", sellIn, quality) };
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         Magasin app = new Magasin(items);
         app.updateQuality();
+        assertEquals(name, app.items[0].name);
         assertEquals(sellIn, app.items[0].sellIn);
         assertEquals(quality, app.items[0].quality);
-        assertEquals("Kryptonite", app.items[0].name);
     }
 }
